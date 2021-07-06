d28887d4d07037931ddc887f06c50df38d796ada
Andres Rodriguez <andy@192.168.1.11>
2021-07-06 19:29:42 -0300

    global configuration added

---
+++ game.js
@@ -0,0 +1,75 @@
+var game;
+
+var gameOptions = {
+  tileSize: 200,
+  tileSpacing: 20,
+  boardSize: {
+    rows: 4,
+    cols: 4,
+  },
+};
+
+window.onload = function() {
+    var gameConfig = {
+        width: gameOptions.boardSize.cols * (gameOptions.tileSize + gameOptions.tileSpacing) + gameOptions.tileSpacing,
+        height: gameOptions.boardSize.rows * (gameOptions.tileSize + gameOptions.tileSpacing) + gameOptions.tileSpacing,
+        backgroundColor: 0xecf0f1,
+        scene: [bootGame, playGame]
+    }
+    game = new Phaser.Game(gameConfig);
+    window.focus();
+    resizeGame();
+    window.addEventListener("resize", resizeGame);
+}
+
+// change size according to where it's used, or window resize --
+function resizeGame() {
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = game.config.width / game.config.height;
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = windowWidth / gameRatio + "px";
+  } else {
+    canvas.style.width = windowHeight * gameRatio + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
+
+// ----------------------------------------------------------
+
+// Scenes --------------------------------------------------
+
+class bootGame extends Phaser.Scene{
+    constructor(){
+        super("BootGame");
+    }
+    preload(){
+        this.load.image("emptytile", "assets/sprites/emptytile.png");
+    }
+    create(){
+        this.scene.start("PlayGame");
+    }
+}
+class playGame extends Phaser.Scene{
+    constructor(){
+        super("PlayGame");
+    }
+    create(){
+        for(var i = 0; i < gameOptions.boardSize.rows; i++){
+            for(var j = 0; j < gameOptions.boardSize.cols; j++){
+                var tilePosition = this.getTilePosition(i, j);
+                this.add.image(tilePosition.x, tilePosition.y, "emptytile");
+            }
+        }
+    }
+    getTilePosition(row, col){
+        var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5);
+        var posY = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
+        return new Phaser.Geom.Point(posX, posY);
+    }
+}
+
+// ----------------------------------------------------------
